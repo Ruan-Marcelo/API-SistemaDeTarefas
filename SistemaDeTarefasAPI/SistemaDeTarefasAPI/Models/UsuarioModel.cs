7a0357b4b5f626ac380fc27a59ad45d18b746770
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-04-03 12:13:30 -0300

    Models Criadas

---
+++ SistemaDeTarefasAPI/SistemaDeTarefasAPI/Models/UsuarioModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaDeTarefasAPI.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+    }
+}
