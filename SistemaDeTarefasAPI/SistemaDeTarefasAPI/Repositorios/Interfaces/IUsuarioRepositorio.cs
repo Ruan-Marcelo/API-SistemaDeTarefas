d93f29ba4295aa9bfa83f44f244f4821e09b4a78
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-04-03 12:45:09 -0300

    Interface com CRUD criada

---
+++ SistemaDeTarefasAPI/SistemaDeTarefasAPI/Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using SistemaDeTarefasAPI.Models;
+
+namespace SistemaDeTarefasAPI.Repositorios.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();
+        Task<UsuarioModel> BuscarPorId(int id);
+        Task<UsuarioModel> Adiconar(UsuarioModel usuario);
+        Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id);
+        Task<bool> Apagar(int id);
+    }
+}
