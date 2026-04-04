638eebeb20728476348e0f68b1222c5042a4bfe9
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-04-04 02:12:47 -0300

    organização para utilizar API via cep

---
+++ SistemaDeTarefasAPI/SistemaDeTarefasAPI/Controllers/CepController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaDeTarefasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+    }
+}
---
+++ SistemaDeTarefasAPI/SistemaDeTarefasAPI/Controllers/Integracao/Response/ViaCepResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace SistemaDeTarefasAPI.Controllers.Integracao.Response
+{
+    public class ViaCepResponse
+    {
+        public string? Cep {  get; set; }
+
+        public string? Locardouro { get; set; }
+
+        public string? Complemento { get; set; }
+
+        public string? Bairro { get; set; }
+
+        public string? Localidade { get; set; }
+
+        public string? Uf { get; set; }
+
+        public string? Ibge { get; set; }
+
+        public string? Gia { get; set; }
+
+        public string? Ddd { get; set; }
+
+        public string? Siafi { get; set; }
+    }
+}
