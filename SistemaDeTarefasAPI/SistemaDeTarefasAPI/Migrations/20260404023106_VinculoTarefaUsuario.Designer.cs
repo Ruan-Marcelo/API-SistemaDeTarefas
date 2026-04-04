bf7fdb2f867814d82503a58d36f7298ec2100357
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-04-03 23:34:50 -0300

    Vinculo de tarefas e usuario e rodando migration

--- SistemaDeTarefasAPI/SistemaDeTarefasAPI/Migrations/20260404023106_VinculoTarefaUsuario.Designer.cs
+++ SistemaDeTarefasAPI/SistemaDeTarefasAPI/Migrations/20260404023106_VinculoTarefaUsuario.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaDeTarefasAPI.Data;
 
@@ -10,9 +11,11 @@ using SistemaDeTarefasAPI.Data;
 namespace SistemaDeTarefasAPI.Migrations
 {
     [DbContext(typeof(SistemaTarefasDBContex))]
-    partial class SistemaTarefasDBContexModelSnapshot : ModelSnapshot
+    [Migration("20260404023106_VinculoTarefaUsuario")]
+    partial class VinculoTarefaUsuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
