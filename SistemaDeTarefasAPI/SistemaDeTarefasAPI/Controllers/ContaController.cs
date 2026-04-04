7014de8eac9d1d5132a530dc8fac85d30f73cc9d
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-04-04 14:52:19 -0300

    gerando token e autenticação ainda mais

--- SistemaDeTarefasAPI/SistemaDeTarefasAPI/Controllers/ContaController.cs
+++ SistemaDeTarefasAPI/SistemaDeTarefasAPI/Controllers/ContaController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using SistemaDeTarefasAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace SistemaDeTarefasAPI.Controllers
 {
@@ -13,9 +17,36 @@ namespace SistemaDeTarefasAPI.Controllers
         {
             if(login.Login == "admin" && login.Senha == "admin")
             {
-                return Ok(new { token = "xpto"});
+                var token = GerarTokenJWT();
+                return Ok(new { token });
             }
+
+
             return BadRequest(new { mensagem = "Credenciais invalidas ou incorretas, verifique sua senha e login novamente"});
+        }
+
+        private string GerarTokenJWT()
+        {
+            string chaveSecreta = "a5ee28f6-fe3a-4342-97a0-7145ab5cf1d0";
+
+            var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(chaveSecreta));
+            var credencial = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim("Login","admin"),
+                 new Claim("nome","Administrador do sistema"),
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "Sua_empresa",
+                audience: "Sua_aplicacao",
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credencial
+             );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
