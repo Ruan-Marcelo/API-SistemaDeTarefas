b5c61b89b303937a462cf131b8c1a2d07d22014e
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-04-04 02:18:43 -0300

    Refit feito url  da requisição da api criada

---
+++ SistemaDeTarefasAPI/SistemaDeTarefasAPI/Controllers/Integracao/Refit/IViaCepIntegracaoRefit.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using SistemaDeTarefasAPI.Controllers.Integracao.Response;
+
+namespace SistemaDeTarefasAPI.Controllers.Integracao.Refit
+{
+    public interface IViaCepIntegracaoRefit
+    {
+        [Get("/ws/{cep}/json")]
+        Task<ApiResponse<ViaCepResponse>> ObterDadosViaCep(string cep);
+    }
+}
