a0e2eeb600f1e7d3a1423a814212b6fb2fa62e96
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-04-08 20:14:09 -0300

    sql

---
+++ SistemaDeTarefas.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Usuario(
+id INT PRIMARY KEY ,
+nome VARCHAR(100),
+email VARCHAR(100)
+);
+
+CREATE TABLE Tarefa(
+id INT PRIMARY KEY ,
+nome VARCHAR(100),
+descricao VARCHAR(100),
+status int
+);
